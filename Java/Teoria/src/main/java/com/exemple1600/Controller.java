e3db2d1f3def0946c0a72f50aa3574cb1d0a7d27
Victor <asensiovector@gmail.com>
2025-03-21 18:30:59 +0100

    .

--- Java/Teoria/src/main/java/com/exemple1600/Controller.java
+++ Java/Teoria/src/main/java/com/exemple1600/Controller.java
@@ -1,11 +1,11 @@
 package com.exemple1600;
 
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.HashMap;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javafx.event.ActionEvent;
@@ -13,18 +13,17 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 
 public class Controller {
 
     @FXML
-    private Button buttonSave;
+    private Button buttonSave, buttonClean, buttonLoad;
 
     @FXML
-    private Button buttonClean;
-
-    @FXML
-    private Button buttonLoad;
+    private File lastOpenedFile;
 
     @FXML
     private TextField titleText;
@@ -32,38 +31,89 @@ public class Controller {
     @FXML
     private TextArea textArea;
 
+    // Carrega un arxiu .json a un quadre de text tipus "TextArea"
     @FXML
-    private void saveAction(ActionEvent event) {
-        String title = titleText.getText(); 
-        String text = textArea.getText();
-        try{
-            HashMap<String, Object> textMap = new HashMap<>();
-            textMap.put("title", title); 
-            textMap.put("text", text);
-            JSONObject JsonObject = new JSONObject(textMap);
-            Files.write(Paths.get("./data/text.json"), JsonObject.toString(4).getBytes());
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
+    private void actionLoadJSON() {
+        Stage stage = (Stage) buttonLoad.getScene().getWindow();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Arxius JSON", "*.json"));
+        File selectedFile = fileChooser.showOpenDialog(stage);
+        if (selectedFile != null) {
+            try {
+                String content = new String(Files.readAllBytes(selectedFile.toPath()));
+                textArea.setText(content);
+                lastOpenedFile = selectedFile; // Guarda el archivo abierto
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
+    //Guarda l'arxiu
     @FXML
-    private void fillAction(ActionEvent event) {
-        try {
-            String content = new String(Files.readAllBytes(Paths.get("./data/text.json")));
+    private void actionSaveJSON() {
+        Stage stage = (Stage) buttonSave.getScene().getWindow();
+        FileChooser fileChooser = new FileChooser();
 
-            JSONObject jsonObject = new JSONObject(content);
+        // Usa el directorio del último archivo abierto, si existe
+        if (lastOpenedFile != null) {
+            fileChooser.setInitialDirectory(lastOpenedFile.getParentFile());
+        } else {
+            fileChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
+        }
 
-            String title = jsonObject.getString("title");
-            String text = jsonObject.getString("text");
-
-            // titleText.setText(title);
-            textArea.setText("Title: " + title + "\nText: " + text);
-
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Arxius JSON", "*.json"));
+        File selectedFile = fileChooser.showSaveDialog(stage);
+        if (selectedFile != null) {
+            try {
+                String jsonData = textArea.getText();
+                if (jsonData.substring(0, 1).equalsIgnoreCase("[")) {
+                    JSONArray json = new JSONArray(jsonData);
+                    Files.write(selectedFile.toPath(), json.toString(4).getBytes());
+                } else {
+                    JSONObject json = new JSONObject(jsonData);
+                    Files.write(selectedFile.toPath(), json.toString(4).getBytes());
+                }
+                lastOpenedFile = selectedFile; // Actualiza el archivo guardado
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
+
+    // @FXML
+    // private void saveAction(ActionEvent event) {
+    //     String title = titleText.getText(); 
+    //     String text = textArea.getText();
+    //     try{
+    //         HashMap<String, Object> textMap = new HashMap<>();
+    //         textMap.put("title", title); 
+    //         textMap.put("text", text);
+    //         JSONObject JsonObject = new JSONObject(textMap);
+    //         Files.write(Paths.get("./data/text.json"), JsonObject.toString(4).getBytes());
+    //     } catch (IOException e) {
+    //         System.err.println(e.getMessage());
+    //     }
+    // }
+
+    // @FXML
+    // private void fillAction(ActionEvent event) {
+    //     try {
+    //         String content = new String(Files.readAllBytes(Paths.get("./data/text.json")));
+
+    //         JSONObject jsonObject = new JSONObject(content);
+
+    //         String title = jsonObject.getString("title");
+    //         String text = jsonObject.getString("text");
+
+    //         // titleText.setText(title);
+    //         textArea.setText("Title: " + title + "\nText: " + text);
+
+    //     } catch (IOException e) {
+    //         System.err.println(e.getMessage());
+    //     }
+    // }
 
     @FXML
     private void cleanAction(ActionEvent event){
